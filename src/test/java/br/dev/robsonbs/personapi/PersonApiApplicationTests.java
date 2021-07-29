dbaca30c64bbfa61c295e6ac44abafb53f4891af
Robson Barbosa Souza <powerrob92@gmail.com>
2021-07-29 15:06:37 -0300

    chore: first commit

---
+++ src/test/java/br/dev/robsonbs/personapi/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.robsonbs.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
